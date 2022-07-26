e69f5cebb0e5b31579d5eeaf466dc9c7d2260c67
Mark Viti <+>
2022-07-26 12:29:05 -0400

    Data base established

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :replies
+  resources :posts
+  resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
